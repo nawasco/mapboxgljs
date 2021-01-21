7b161c591d8e5802c49879d4b2e71a0f2faae74a
james kirumba <fullstop4039@gmail.com>
2021-01-21 17:48:51 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,8 +6,9 @@ module.exports = {
     attribution : '©Nanyuki Water and Serwerage Co., Ltd.',
     // change stylefiles URLs to yours
     styles : [
-        { title: 'Street(water)', uri: `https://nawasco.github.io/mapbox-stylefiles/street/style.json?version=${version}`,}, 
+       
         { title: 'Satellite(water)', uri: `https://nawasco.github.io/mapbox-stylefiles/satellite/style.json?version=${version}`,},
+         { title: 'Street(water)', uri: `https://nawasco.github.io/mapbox-stylefiles/street/style.json?version=${version}`,}, 
         { title: 'Street(sewer)', uri: `https://nawasco.github.io/mapbox-stylefiles/street/style_sewer.json?version=${version}`,}, 
         { title: 'Satellite(sewer)', uri: `https://nawasco.github.io/mapbox-stylefiles/satellite/style_sewer.json?version=${version}`,},
         { title: 'UN Vector Tile', uri: `https://nawasco.github.io/mapbox-stylefiles/unvt/style.json?version=${version}`,},
